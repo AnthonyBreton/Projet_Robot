9407c326cff73d1424c6915cfd315ada0808a1c7
Diego-Alonso L.R <Diego-Alonso.Leblanc-Romero@USherbrooke.ca>
2019-10-10 10:02:12 -0400

    Cochonnerie

--- Defi_Du_Parcours/src/main.ino
+++ Defi_Du_Parcours/src/main.ino
@@ -20,17 +20,98 @@ void setup()
 {
   BoardInit();
   delay(1000);
-/*
-  Tourner(-90);
-  delay(1000);
-  Tourner(90);
-  delay(1000);
-  Tourner(-180);
-  delay(1000);
-  Tourner(180);
-  delay(1000);
-*/
+  Serial.begin(9600);
 
+  Avancer(115);
+  delay(200);
+ 
+  Tourner(-86);
+  delay(200);
+  
+  Avancer(81);
+  delay(200);
+ 
+  Tourner(94);
+  delay(200);
+
+  Avancer(26);
+  delay(200);
+
+  Tourner(92);
+  delay(200);
+
+  Avancer(30);
+  delay(200);
+
+  Tourner(-88);
+  delay(200);
+
+  Avancer(100);
+  delay(200);
+
+  Tourner(2);
+  delay(200);
+
+  Avancer(91);
+  delay(200);
+
+  Tourner(92);
+  delay(200);
+
+  Avancer(15);//11
+  delay(200);
+
+  Tourner(-88);
+  delay(200);
+
+  Avancer(120);
+  delay(200);
+
+  UTurn();
+  delay(500);
+  
+  Avancer(120);
+  delay(200);
+
+  Tourner(92);
+  delay(200);
+
+  Avancer(15);//11 b
+  delay(200);
+
+  Tourner(-88);
+  delay(200);
+
+  Avancer(191);
+  delay(200);
+
+  Tourner(92);
+  delay(200);
+
+  Avancer(30);
+  delay(200);
+
+  Tourner(-88);
+  delay(200);
+
+  Avancer(26);
+  delay(200);
+
+  Tourner(-88);
+  delay(200);
+
+  Avancer(81);
+  delay(200);
+
+  Tourner(92);
+  delay(200);
+
+  Avancer(115);
+
+  UTurn();
+  UTurn();
+
+/* ANCIENNE SECTION
   Avancer(116);
   delay(100);
 
@@ -101,7 +182,7 @@ void setup()
   delay(100);
 
   UTurn();UTurn();UTurn();UTurn();UTurn();
-  
+  */
 }
 
 
@@ -121,7 +202,8 @@ void Avancer(float distance)
     int32_t nbPulse = 0, nbPulseM = 0, nbPulseE = 0; //M=maitre, E=esclave
     float circonference = 23.938936; //Diamètre des roues en cm * Pi
     int erreurL = 0, erreurT=0; //erreurL=erreur local, erreurT=erreur total
-    float KP=0.0006 , KI=0.00002;//coefficiant pour erreurL et erreurT
+    float KP=0.002 , KI=0.00022;//coefficiant pour erreurL et erreurT
+    // float KP=0.0006 , KI=0.00002; avant changement 2019/10/10
     //float KP=0.0004 , KI=0.00002; avant changement 2019/10/09
     //2019-10-02 avant changement: KP=0.0001, KI=0.00002
     //Trouver le nombre de pulse nécessaire pour une distance donnée en cm
@@ -140,13 +222,13 @@ void Avancer(float distance)
       vE = vM;
       MOTOR_SetSpeed(0, vM);
       MOTOR_SetSpeed(1, vE);
-      delay(15);
+      delay(4);
     }
 
     //Permet de réduire la vitesse juste avant la fin pour éviter un arrêt brusque
     while (compteurM < (nbPulse-3200))
     {
-        delay(100);
+        delay(10);
         nbPulseM = ENCODER_Read(0);
         nbPulseE = ENCODER_Read(1);
 
@@ -155,6 +237,9 @@ void Avancer(float distance)
         compteurM+= nbPulseM; 
         compteurE+= nbPulseE; 
         erreurT =compteurM-compteurE;
+        Serial.print(erreurL);
+        Serial.print("    ");
+        Serial.println(erreurT);
         //calcul de la nouvelle vitesse pour vE
         vE= vE + erreurL * KP + erreurT * KI;
         MOTOR_SetSpeed(1,vE);
@@ -170,13 +255,13 @@ void Avancer(float distance)
       vE = vM;
       MOTOR_SetSpeed(0, vM);
       MOTOR_SetSpeed(1, vE);
-      delay(10);
+      delay(5);
     }
 
     //Détermine quand le robot à atteint la distance pour l'arrêter
     while (compteurM < nbPulse)
     {
-        delay(100);
+        delay(10);
         nbPulseM = ENCODER_Read(0);
         nbPulseE = ENCODER_Read(1);
 
