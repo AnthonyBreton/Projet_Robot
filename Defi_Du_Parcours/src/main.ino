342467e85da701c6afa39e36f4d1294995afd7aa
Raphaël Cloutier <clor2504@usherbrooke.ca>
2019-10-10 10:02:02 -0400

    Parcours
    Ajout des valeurs pour le parcours et ajout d'une focntion sans accelération

--- Defi_Du_Parcours/src/main.ino
+++ Defi_Du_Parcours/src/main.ino
@@ -19,104 +19,91 @@ void setup()
 {
   BoardInit();
   Serial.begin(9600);
- // Tourner(360);
-  //delay(1000);
-  //Tourner(-360);
-  //Serial.begin(9600);
-/*
-  Tourner(-90);
-  delay(1000);
-  Tourner(90);
-  delay(1000);
-  Tourner(-180);
-  delay(1000);
-  Tourner(180);
-  delay(1000);
-*/
 
-//Avancer(300);
-//delay(100);
+  Avancer(115);
+  delay(200);
  
-
- 
-  Avancer(120);
-  delay(200);
-
-  Tourner(-90);
-  delay(200);
-
-  Avancer(70);
-  delay(200);
-
-  Tourner(90);
-  delay(200);
-
-  Avancer(75);
-  delay(200);
-
-  Tourner(41); //45
-  delay(200);
-
-  Avancer(175);
+  Tourner(-87);
   delay(200);
   
-  Tourner(-90);
+  Avancer(81);
+  delay(200);
+ 
+  Tourner(90);
   delay(200);
 
-  Avancer(45);
-  delay(200);
-
-  Tourner(45);
-  delay(200);
-
-  Avancer(100);
-  delay(200);
-
-  //UTURN DAWG
-  UTurn();
-  delay(200);
-  //UTURN DAWG
-
- Avancer(100); 
-  delay(200);
-
-  Tourner(-45);
-  delay(200);
-
-  Avancer(45);
+  Avancer2(26);
   delay(200);
 
   Tourner(90);
-  
   delay(200);
 
-  Avancer(175);
-  delay(200);
-
-  Tourner(-45);
-  delay(200);
-
-  Avancer(75);
+  Avancer(30);
   delay(200);
 
   Tourner(-87);
   delay(200);
 
-  Avancer(71);
+  Avancer(191);
   delay(200);
 
-  Tourner(92);
+  Tourner(90);
   delay(200);
 
-  Avancer(130);
+  Avancer2(15);//11
+  delay(200);
+
+  Tourner(-87);
+  delay(200);
+
+  Avancer(120);
   delay(200);
 
   UTurn();
-  UTurn();
-  delay(2000);
-  UTurn();
-  UTurn();
+  delay(500);
+  
+  Avancer(120);
+  delay(200);
 
+  Tourner(90);
+  delay(200);
+
+  Avancer2(15);//11 b
+  delay(200);
+
+  Tourner(-87);
+  delay(200);
+
+  Avancer(191);
+  delay(200);
+
+  Tourner(90);
+  delay(200);
+
+  Avancer(30);
+  delay(200);
+
+  Tourner(-87);
+  delay(200);
+
+  Avancer2(26);
+  delay(200);
+
+  Tourner(-87);
+  delay(200);
+
+  Avancer(81);
+  delay(200);
+
+  Tourner(90);
+  delay(200);
+
+  Avancer(115);
+
+  UTurn();
+  UTurn();
+  
+ 
 }
 
 
@@ -127,6 +114,96 @@ void loop()
   
 }
 
+void Avancer2(float distance)
+{
+    float vM=0.5;
+    float vE=vM;
+    int compteurM = 0, compteurE = 0; //nb de pulse lu depuis de le debut
+    int32_t nbPulse = 0, nbPulseM = 0, nbPulseE = 0; //M=maitre, E=esclave
+    float circonference = 23.938936; //Diamètre des roues en cm * Pi
+    int erreurL = 0, erreurT=450; //erreurL=erreur local, erreurT=erreur total
+    float KP=0.0007 , KI=0.00020;//coefficiant pour erreurL et erreurT
+    //2019-10-09 avant changement: KP=0.0001, KI=0.00002
+    //Trouver le nombre de pulse nécessaire pour une distance donnée en cm
+    nbPulse = distance / circonference * 3200;
+
+    //Réinitialise les compteurs de pulses
+    ENCODER_ReadReset(0);
+    ENCODER_ReadReset(1);
+
+    
+
+    //Initialiser la vitesse des deux moteurs à 50%
+    MOTOR_SetSpeed(0, vM);
+    MOTOR_SetSpeed(1, vE);
+
+    
+
+    //Permet de réduire la vitesse juste avant la fin pour éviter un arrêt brusque
+    
+    while (compteurM < (nbPulse-3200))
+    {
+        delay(100);
+        nbPulseM = ENCODER_Read(0);
+        nbPulseE = ENCODER_Read(1);
+
+        //on trouve les erreur L et T
+        erreurL= nbPulseM-nbPulseE;
+        //Serial.println(erreurL);
+        compteurM += nbPulseM; //SI sa fonctionne pas, on fait la moyenne des 2 pulse (M et E)
+        compteurE += nbPulseE;
+        erreurT = compteurM-compteurE;
+        Serial.print(erreurL);
+        Serial.print("    ");
+        Serial.println(erreurT);
+        //calcul de la nouvelle vitesse pour vE
+        vE= vM + erreurL * KP + erreurT * KI;
+        MOTOR_SetSpeed(1,vE);
+
+        //On reset les lectures des pulses
+        ENCODER_ReadReset(0);
+        ENCODER_ReadReset(1);
+    }
+
+    //Réduit la vitesse des moteurs
+    while (vM >= 0.2)
+    {
+      vM -= 0.01;
+      vE = vM;
+      MOTOR_SetSpeed(0, vM);
+      MOTOR_SetSpeed(1, vE);
+      delay(10);
+    }
+
+    //Détermine quand le robot à atteint la distance pour l'arrêter
+    while (compteurM < nbPulse)
+    {
+        delay(100);
+        nbPulseM = ENCODER_Read(0);
+        nbPulseE = ENCODER_Read(1);
+
+        //on trouve les erreur L et T
+        erreurL= nbPulseM-nbPulseE;
+        compteurM+= nbPulseM; //SI sa fonctionne pas, on fait la moyenne des 2 pulse (M et E)
+        compteurE += nbPulseE;
+        erreurT += compteurM - compteurE;
+
+        //calcul de la nouvelle vitesse pour vE
+        vE= vE + erreurL * KP + (erreurT) * KI;
+        MOTOR_SetSpeed(1,vE);
+
+        //On reset les lectures des pulses
+        ENCODER_ReadReset(0);
+        ENCODER_ReadReset(1);
+    }
+
+  
+
+    //Arrête les moteurs
+    vE=vM=0;
+    MOTOR_SetSpeed(0, vM);
+    MOTOR_SetSpeed(1, vE);
+}
 
 void Avancer(float distance)
 {
